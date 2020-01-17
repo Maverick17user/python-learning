b3358d3d2b7d6e714098fc4ffd0f86e6eed60f78
Yepifanau Maksim <30416868+Maverick17user@users.noreply.github.com>
2020-01-17 14:38:40 +0300

    Update sum.py

--- 1/sum.py
+++ 1/sum.py
@@ -2,7 +2,7 @@ def matematichka(n):
     if n > 1:
         summ = 0
         for count in range(n):
-            summ += 1. / (2 ** count + 1)
+            summ += 1. / (2 ** count)
         return summ
     else:
         print('n must be more then 1.')
